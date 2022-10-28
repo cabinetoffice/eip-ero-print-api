e8983742cc7bf449718665799154f2c5753a5b7f
Rob O'Rorke <rob.o'rorke@valtech.com>
2022-10-28 17:32:46 +0100

    EIP1-2265 : implement sftp server

--- src/test/kotlin/uk/gov/dluhc/printapi/config/SftpContainerConfiguration.kt
+++ src/test/kotlin/uk/gov/dluhc/printapi/config/SftpContainerConfiguration.kt
@@ -1,11 +1,11 @@
 package uk.gov.dluhc.printapi.config
 
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Lazy
 import org.testcontainers.containers.BindMode
 import org.testcontainers.containers.GenericContainer
 import org.testcontainers.images.builder.ImageFromDockerfile
+import java.net.URL
+
 
 /**
  * Configuration class exposing an sftp service.
@@ -15,14 +15,22 @@ class SftpContainerConfiguration {
     companion object {
         private const val USER = "valtech"
         private const val PASSWORD = "Password"
+        private const val PUBLIC_KEY_FILENAME = "ssh/id_rsa.pub"
+        private const val PRIVATE_KEY_FILENAME = "ssh/printer_rsa"
         private const val PORT = 22
-        private const val IMAGE_NAME = "atmoz/sftp:latest"
+        private const val IMAGE_NAME = "atmoz/sftp:debian"
         private const val USER_ID = 1001
         private const val GROUP_ID = 100
         private val DIRECTORIES = listOf("EROP/Dev/InBound", "EROP/Dev/OutBound")
         private var container: GenericContainer<*>? = null
 
+        private fun getPublicKeyResourceUrl(): URL? = ClassLoader.getSystemResource(PUBLIC_KEY_FILENAME)
+
+        fun getPrivateKeyResourceUrl(): URL? = ClassLoader.getSystemResource(PRIVATE_KEY_FILENAME)
+
         fun getInstance(): GenericContainer<*> {
+            val publicKeyResourceUrl: URL = getPublicKeyResourceUrl()!!
+
             if (container == null) {
                 container = GenericContainer(
                         ImageFromDockerfile().withDockerfileFromBuilder {
@@ -30,7 +38,7 @@ class SftpContainerConfiguration {
                                     .build()
                         },
                 )
-                        .withFileSystemBind("E:\\IdeaProjects\\eip-ero-print-api\\src\\test\\resources\\ssh\\id_rsa.pub", "/home/$USER/.ssh/keys/id_rsa.pub", BindMode.READ_ONLY)
+                        .withFileSystemBind(publicKeyResourceUrl.file, "/home/$USER/.ssh/keys/id_rsa.pub", BindMode.READ_ONLY)
                         .withExposedPorts(PORT)
                         .withCommand("$USER:$PASSWORD:$USER_ID:$GROUP_ID:${DIRECTORIES.joinToString(",")}")
             }
--- src/test/kotlin/uk/gov/dluhc/printapi/rest/SftpIntegrationTest.kt
+++ src/test/kotlin/uk/gov/dluhc/printapi/rest/SftpIntegrationTest.kt
@@ -1,6 +1,7 @@
 package uk.gov.dluhc.printapi.rest
 
 import mu.KotlinLogging
+import org.apache.commons.io.FileUtils
 import org.junit.jupiter.api.Test
 import uk.gov.dluhc.printapi.config.SftpContainerConfiguration
 
@@ -12,9 +13,19 @@ internal class SftpIntegrationTest {
     fun `should return health check status UP given microservice is running healthily`() {
         // Given
         val sftpInstance = SftpContainerConfiguration.getInstance()
-        sftpInstance.start();
-        val mappedPort = sftpInstance.getMappedPort(22)
+        sftpInstance.start()
 
-        logger.info { "mapped port = $mappedPort" }
+        val mappedPort = sftpInstance.getMappedPort(22)
+        val privateKeyResourceUrl = SftpContainerConfiguration.getPrivateKeyResourceUrl()
+
+        logger.info { "Change directory to that containing the private key and then run the sftp command." }
+        logger.info { "cd ${FileUtils.toFile(privateKeyResourceUrl).parent}" }
+        logger.info { "sftp -v -P $mappedPort -oKexAlgorithms=+diffie-hellman-group1-sha1 -o \"IdentityFile=./printer_rsa\" -o User=valtech localhost" }
+        logger.info { "You can now verify that the 2 required directories exist by running the following command" }
+        logger.info { """
+            sftp> ls EROP/Dev
+            EROP/Dev/InBound   EROP/Dev/OutBound  
+            sftp> 
+        """.trimIndent() }
     }
 }
