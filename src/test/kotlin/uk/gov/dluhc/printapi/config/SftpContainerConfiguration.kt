de3d36c1742f4dcbe2c3fdbb366222164edef613
Rob O'Rorke <rob.o'rorke@valtech.com>
2022-10-30 15:19:27 +0000

    EIP1-2265 : working test

--- src/test/kotlin/uk/gov/dluhc/printapi/config/SftpContainerConfiguration.kt
+++ src/test/kotlin/uk/gov/dluhc/printapi/config/SftpContainerConfiguration.kt
@@ -24,28 +24,30 @@ class SftpContainerConfiguration {
         val DIRECTORIES = listOf("EROP/Dev/InBound", "EROP/Dev/OutBound")
         var container: GenericContainer<*>? = null
 
-        fun remoteDirectory() = "sftp://$USERNAME@localhost:${getMappedPort()}/${DIRECTORIES[0]}"
+        fun getConnectionUrl() = "sftp://$USERNAME@localhost:${getMappedPort()}/${DIRECTORIES[0]}"
 
         fun getPublicKeyResourceUrl(): URL = ClassLoader.getSystemResource(PUBLIC_KEY_FILENAME)
 
         fun getPrivateKeyResourceUrl(): URL = ClassLoader.getSystemResource(PRIVATE_KEY_FILENAME)
 
         fun getInstance(): GenericContainer<*> {
-            val publicKeyResourceUrl: URL = getPublicKeyResourceUrl()
+            synchronized(this) {
+                val publicKeyResourceUrl: URL = getPublicKeyResourceUrl()
 
-            if (container == null) {
-                container = GenericContainer(
+                if (container == null) {
+                    container = GenericContainer(
                         ImageFromDockerfile().withDockerfileFromBuilder {
                             it.from(IMAGE_NAME)
-                                    .build()
+                                .build()
                         },
-                )
+                    )
                         .withFileSystemBind(publicKeyResourceUrl.file, "/home/$USERNAME/.ssh/keys/id_rsa.pub", BindMode.READ_ONLY)
                         .withExposedPorts(PORT)
                         .withCommand("$USERNAME:$PASSWORD:$USER_ID:$GROUP_ID:${DIRECTORIES.joinToString(",")}")
+                    container!!.start()
+                }
+                return container!!
             }
-            container!!.start()
-            return container!!
         }
 
         fun getMappedPort() = container!!.getMappedPort(22)
--- src/test/kotlin/uk/gov/dluhc/printapi/rest/SftpIntegrationTest.kt
+++ src/test/kotlin/uk/gov/dluhc/printapi/rest/SftpIntegrationTest.kt
@@ -4,23 +4,13 @@ import com.jcraft.jsch.Channel
 import com.jcraft.jsch.ChannelSftp
 import com.jcraft.jsch.ChannelSftp.LsEntry
 import com.jcraft.jsch.JSch
-import com.jcraft.jsch.JSchException
-import com.jcraft.jsch.Session
-import com.jcraft.jsch.SftpException
 import mu.KotlinLogging
 import org.apache.commons.io.FileUtils
-import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import uk.gov.dluhc.printapi.config.SftpContainerConfiguration
 import uk.gov.dluhc.printapi.config.SftpContainerConfiguration.Companion.USERNAME
 import java.net.URL
-import java.nio.file.Files
-import java.nio.file.Paths
-import java.security.KeyFactory
-import java.security.PrivateKey
-import java.security.spec.PKCS8EncodedKeySpec
-import java.util.Properties
 import java.util.Vector
 
 
@@ -36,66 +26,75 @@ internal class SftpIntegrationTest {
     fun `should return health check status UP given microservice is running healthily`() {
         // Given
         val privateKeyResourceUrl: URL = SftpContainerConfiguration.getPrivateKeyResourceUrl()
-
-//        val key: ByteArray = Files.readAllBytes(Paths.get(privateKeyResourceUrl.file))
-//        val keyFactory: KeyFactory = KeyFactory.getInstance("RSA")
-//        val keySpec = PKCS8EncodedKeySpec(key)
-//        val finalKey: PrivateKey = keyFactory.generatePrivate(keySpec)
-//        println(finalKey.algorithm)
-
         val sftpInstance = SftpContainerConfiguration.getInstance()
-        val mappedPort = sftpInstance.getMappedPort(22)
+        val mappedPort: Int = sftpInstance.getMappedPort(22)
+
+        outputConnectionString(mappedPort, privateKeyResourceUrl)
+
+        val jsch = createJschWithPrivateKey(privateKeyResourceUrl)
+        val response = getResponse(jsch, mappedPort)
+
+        logger.info { "ls = $response" }
+        assertThat(response).hasSize(4)
+        assertThat(response).anyMatch { it.filename == "InBound" }
+        assertThat(response).anyMatch { it.filename == "OutBound" }
+    }
+
+    private fun getResponse(jsch: JSch, mappedPort: Int): Vector<LsEntry> {
+        jsch.getSession(USERNAME, "localhost", mappedPort).run {
+            val runCommandResponse: Vector<LsEntry>?
+            try {
+                setConfig("PreferredAuthentications", "publickey,keyboard-interactive,password")
+                setConfig("StrictHostKeyChecking", "no")
+                connect(SESSION_TIMEOUT)
+                val sftp: Channel = openChannel("sftp")
+                runCommandResponse = runCommand(sftp as ChannelSftp)
+            } finally {
+                disconnect()
+            }
+            return runCommandResponse!!
+        }
+    }
+
+    private fun createJschWithPrivateKey(privateKeyResourceUrl: URL): JSch {
+        val jsch = JSch()
+        val privateKeyPath = privateKeyResourceUrl.path
+        jsch.addIdentity(privateKeyPath)
+        return jsch
+    }
+
+    private fun runCommand(sftp: ChannelSftp): Vector<LsEntry> {
+        try{
+            sftp.connect(CHANNEL_TIMEOUT)
+            return doCommand(sftp)
+        } finally {
+            sftp.exit()
+        }
+    }
+
+    private fun doCommand(channelSftp: ChannelSftp): Vector<LsEntry> = channelSftp.ls("/EROP/Dev") as Vector<LsEntry>
+
+    private fun outputConnectionString(
+        mappedPort: Int,
+        privateKeyResourceUrl: URL,
+    ) {
+        val cmd = "sftp -v -P $mappedPort " +
+                "-oStrictHostKeyChecking=no " +
+                "-oKexAlgorithms=+diffie-hellman-group1-sha1 " +
+                "-o 'IdentityFile=/home/valtech/IdeaProjects/eip-ero-print-api/build/resources/test/ssh/printer_rsa' " +
+                "-o User=valtech " +
+                "localhost"
 
         logger.info { "Change directory to that containing the private key and then run the sftp command." }
         logger.info { "cd ${FileUtils.toFile(privateKeyResourceUrl).parent}" }
-        logger.info { "sftp -v -P $mappedPort -oKexAlgorithms=+diffie-hellman-group1-sha1 -o \"IdentityFile=./printer_rsa\" -o User=valtech localhost" }
+        logger.info { cmd }
         logger.info { "You can now verify that the 2 required directories exist by running the following command" }
         logger.info {
             """
-            sftp> ls EROP/Dev
-            EROP/Dev/InBound   EROP/Dev/OutBound  
-            sftp> 
-        """.trimIndent()
-        }
-
-        val jsch = JSch()
-        var jschSession: Session? = null
-        val privateKeyPath = privateKeyResourceUrl.path
-        try {
-            jsch.addIdentity(privateKeyPath)
-            jschSession = jsch.getSession(USERNAME, "localhost", mappedPort)
-            jschSession.setConfig("PreferredAuthentications", "publickey,keyboard-interactive,password")
-            val config = Properties()
-            config["StrictHostKeyChecking"] = "no"
-            jschSession.setConfig(config)
-        } catch (e: JSchException) {
-            throw RuntimeException("Failed to create Jsch Session object.", e)
-        }
-
-        try {
-            // 10 seconds session timeout
-            jschSession.connect(SESSION_TIMEOUT)
-            val sftp: Channel = jschSession.openChannel("sftp")
-
-            // 5 seconds timeout
-            sftp.connect(CHANNEL_TIMEOUT)
-            val channelSftp: ChannelSftp = sftp as ChannelSftp
-
-            // transfer file from local to remote server
-            val ls: Vector<LsEntry> = channelSftp.ls("/EROP/Dev") as Vector<LsEntry>
-            logger.info { "ls = $ls" }
-
-            assertThat(ls).hasSize(4)
-            assertThat(ls).anyMatch { it.filename == "InBound" }
-            assertThat(ls).anyMatch { it.filename == "OutBound" }
-
-            // download file from remote server to local
-            // channelSftp.get(remoteFile, localFile);
-            channelSftp.exit()
-        } finally {
-            if (jschSession != null) {
-                jschSession.disconnect()
-            }
+                sftp> ls EROP/Dev
+                EROP/Dev/InBound   EROP/Dev/OutBound  
+                sftp> 
+            """.trimIndent()
         }
     }
 }
